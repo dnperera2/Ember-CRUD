db1c70974d1636f1093899f8946da693363a04f1
dnperera2 <dnperera@mitrai.com>
2025-10-16 15:15:20 +0530

    Revert "CRUD done"
    This reverts commit 7378e54f7b72f0363de781956779e2b30269404f.

--- api/src/main/java/boardgame/api/repository/BoardgameRepository.java
+++
@@ -1,6 +0,0 @@
-package boardgame.api.repository;
-
-import boardgame.api.model.Boardgame;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardgameRepository extends JpaRepository<Boardgame, Long> {}
